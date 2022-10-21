60704ba31ec3bf2f117a28c22fdce6fe34ea23a7
Catalina Hermoso <93346821+CataHermoso@users.noreply.github.com>
2022-10-21 18:33:50 -0300

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -13,87 +13,155 @@ struct ContentView: View {
     @Binding var page: String
     @State private var numPage = 1
     
-    
     var body: some View {
         NavigationView {
-            
-            //LIST CHARACTERS
-            api()
-            
-            //TOOLBAR BOTTOM
-            HStack {
-                Text("")
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            if numPage != 1 {
-                                Button("<- Previous") {
-                                    page.removeLast()
-                                    numPage = numPage-1
-                                    page = page+"\(numPage)"
-                                    print(page)
-                                }
+            api() //LIST CHARACTERS
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        if numPage != 1 {
+                            Button {
+                                page.removeLast()
+                                numPage = numPage+1
+                                page = page+"\(numPage)"
+                                print(page)
+                            } label: {
+                                Image("falcon")
+                                    .resizable()
+                                    .frame(width: 40.0, height: 30.0)
+                                
+                            }
+                        }
+                        else if numPage != 9 {
+                            Button {
+                                page.removeLast()
+                                numPage = numPage+1
+                                page = page+"\(numPage)"
+                                print(page)
+                            } label: {
+                                Image("falcon")
+                                    .resizable()
+                                    .frame(width: 40.0, height: 30.0)
                             }
                         }
                     }
-                Spacer()
-                Text("")
-                    .toolbar {
-                        ToolbarItem(placement: .bottomBar) {
-                            if numPage != 9 {
-                                Button("Next ->") {
-                                    page.removeLast()
-                                    numPage = numPage+1
-                                    page = page+"\(numPage)"
-                                    print(page)
-                                }
-                            }
+                }
+        }
+    }
+}
+
+struct api: View { //LIST CHARACTERS AND CONNECTION SHEET
+    @StateObject var viewModel = ViewModel()
+    @State var isSheetPresented = false
+    @State private var isConnectionEnabled = true
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.characters, id: \.self) { pj in
+                NavigationLink (destination: PjView()) {
+                    HStack {
+                        Image(systemName: "person.crop.circle")
+                            .font(.system(size: 27))
+                            .padding(5)
+                            .foregroundColor(Color("darkBlue"))
+                        VStack (alignment: .leading) {
+                            Text(pj.name)
+                                .font(.headline)
+                            Text(pj.gender)
+                                .font(.body)
                         }
                     }
+                }
             }
         }
-    }
-}
-
-struct api: View {
-    @StateObject var viewModel = ViewModel()
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(viewModel.characters, id: \.self) { pj in
-                    VStack {
-                        Text(pj.name)
-                            .font(.headline)
-                        Text(pj.gender)
-                            .font(.body)
+        .navigationTitle("Characters") //make it white
+        .toolbar {
+            Button(action: {isSheetPresented=true}) {
+                Image(systemName: "antenna.radiowaves.left.and.right")
+                    .foregroundColor(.blue) //.yyellow
+            }
+        }
+        .toolbarBackground(.hidden, for: .navigationBar)
+        .onAppear {
+            viewModel.fetchData()
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            NavigationView {
+                ZStack {
+                    //Image("starsMoving")
+                    if (isConnectionEnabled == true) {
+                        VStack {
+                            ZStack {
+                                Rectangle()
+                                    .fill(.white)
+                                    .cornerRadius(10)
+                                    .frame(height: 90, alignment: .center)
+                                VStack (alignment: .center) {
+                                    Text("If you disable the connection,")
+                                    Text("sightings won't be reported.")
+                                        .fontWeight(.bold)
+                                }
+                            }
+                            ZStack {
+                                Rectangle()
+                                    .fill(.yellow)
+                                    .cornerRadius(10)
+                                    .frame(width: 300, height: 100, alignment: .center)
+                                Button (action: {
+                                    isConnectionEnabled=false
+                                    isSheetPresented=false
+                                    presentationMode.wrappedValue.dismiss()
+                                }) {
+                                    Image(systemName: "wifi.slash")
+                                        .font(.system(size: 40))
+                                        .foregroundColor(.black)
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        VStack {
+                            ZStack {
+                                Rectangle()
+                                    .fill(.white)
+                                    .cornerRadius(10)
+                                    .frame(height: 90, alignment: .center)
+                                VStack (alignment: .center) {
+                                    Text("If you enable the connection,")
+                                    Text("sightings will be reported.")
+                                        .fontWeight(.bold)
+                                }
+                            }
+                            ZStack {
+                                Rectangle()
+                                    .fill(.yellow)
+                                    .cornerRadius(10)
+                                    .frame(width: 300, height: 100, alignment: .center)
+                                Button (action: {
+                                    isConnectionEnabled=true
+                                    isSheetPresented=false
+                                    presentationMode.wrappedValue.dismiss()
+                                }) {
+                                    Image(systemName: "wifi")
+                                        .font(.system(size: 40))
+                                        .foregroundColor(.black)
+                                }
+                            }
+                        }
                     }
                 }
-
-            } .navigationTitle("Characters")
-                .onAppear {
-                    viewModel.fetchData()
-                }
+            }
         }
+        //BACKGROUND
+        //.background(Image("starsMoving"))
+//        .background(ZStack {
+//            LinearGradient(gradient: Gradient(colors: [Color("darkBlue"), Color("lightBlue")]), startPoint: .top, endPoint: .bottom)
+//            Image("stars")
+//        })
+//        .scrollContentBackground(.hidden)
     }
 }
-    
-//    func loadData() async {
-//        guard let url = URL(string: page) else {
-//            print("Invalid URL")
-//            return
-//        }
-//
-//        do {
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//
-//            if let decodedResponse =  try? JSONDecoder().decode([Character].self, from: data) {
-//                results = decodedResponse
-//            }
-//        } catch {
-//            print("The data is invalid")
-//        }
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
